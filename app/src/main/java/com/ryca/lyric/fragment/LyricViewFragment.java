02bb83a93e99056ac31393dc320624d16337d049
saeed <hassanisaeed19@yahoo.com>
2020-01-11 15:35:27 +0330

    Add Comment on Bazaar

--- app/src/main/java/com/ryca/lyric/fragment/LyricViewFragment.java
+++ app/src/main/java/com/ryca/lyric/fragment/LyricViewFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
